eab0a98a76efe36fd28fd060b9956901813a45a4
Mateus Bizzo <bizzo.mateus@gmail.com>
2020-01-23 10:17:02 -0300

    Docstring for Post Model

--- blog/models.py
+++ blog/models.py
@@ -4,15 +4,18 @@ from django.utils import timezone
 
 
 class Post(models.Model):
+    """ Post model for blog """
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
+    """ Include actual time zone and save post """
     def publish(self):
         self.published_date = timezone.now()
         self.save()
 
+    """ Returns post title """
     def __str__(self):
         return self.title
