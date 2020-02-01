d77a71d7b1b348ee396ae45759888c69b056e6a2
Mateus Bizzo <bizzo.mateus@gmail.com>
2020-02-01 15:31:22 -0300

    Include TinyMCE for post text handle

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,17 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from tinymce.models import HTMLField
 
 
 class Post(models.Model):
     """ Post model for blog """
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    text = models.TextField()
+    text = HTMLField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    # TODO: Create labels for posts.
 
     def publish(self):
         """ Include actual time zone and save post """
@@ -17,5 +19,5 @@ class Post(models.Model):
         self.save()
 
     def __str__(self):
-        """ Returns post title """
+        """ Returns post title for adm """
         return self.title
--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tinymce',
     'blog',
 ]
 
@@ -114,6 +115,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+TINYMCE_DEFAULT_CONFIG = {
+    'theme': "advanced"
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
